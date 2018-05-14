9c0e7d7d75e2cb110efec05641a87c3b0584e127
Paul Reimer <paul@p-rimes.net>
2018-05-14 09:57:44 -0700

    🎨 Add comments to display_actor when setting up OLED I2C display via u8g2.

--- firmware/main/display_actor.cpp
+++ firmware/main/display_actor.cpp
@@ -15,25 +15,31 @@ struct DisplayActorState
 {
   DisplayActorState()
   {
-    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+    // Configure hardware ESP32-specific HAL settings
+    u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
     u8g2_esp32_hal.sda = pin_sda;
     u8g2_esp32_hal.scl = pin_scl;
     u8g2_esp32_hal.reset = pin_reset;
+
+    // Initialize the ESP32-specific HAL hardware
     u8g2_esp32_hal_init(u8g2_esp32_hal);
 
-    // init u8g2 structure
+    // Initialize u8g2 structure
     u8g2_Setup_ssd1306_128x32_univision_f(
       &u8g2,
       U8G2_R0,
-      //u8x8_byte_sw_i2c,
       u8g2_esp32_msg_i2c_cb,
       u8g2_esp32_msg_i2c_and_delay_cb
     );
-    u8x8_SetI2CAddress(&u8g2.u8x8,0x78);
 
-    u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
+    // Set the I2C slave address
+    u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
 
-    u8g2_SetPowerSave(&u8g2, 0); // wake up display
+   // Send init sequence to the display -- display is in sleep mode after this.
+    u8g2_InitDisplay(&u8g2);
+
+    // Wake up display
+    u8g2_SetPowerSave(&u8g2, 0);
 
     // Send an initial blank frame
     u8g2_ClearBuffer(&u8g2);
@@ -42,6 +48,7 @@ struct DisplayActorState
     u8g2_SendBuffer(&u8g2);
   }
 
+  u8g2_esp32_hal_t u8g2_esp32_hal;
   gpio_num_t pin_sda = static_cast<gpio_num_t>(4);
   gpio_num_t pin_scl = static_cast<gpio_num_t>(15);
   gpio_num_t pin_reset = static_cast<gpio_num_t>(16);
@@ -84,9 +91,13 @@ auto display_actor_behaviour(
         const auto& line1 = show_user_details_action->name();
         const auto& line2 = show_user_details_action->makerlabs_id();
 
+        // Clear the buffer
+        u8g2_ClearBuffer(&u8g2);
+
         // Draw line1: Full Name
         if (line1)
         {
+          printf("%s\n", line1->c_str());
           u8g2_SetFont(&u8g2, u8g2_font_helvR10_tr);
           u8g2_DrawStr(&u8g2, 0, 13, line1->c_str());
         }
@@ -94,9 +105,8 @@ auto display_actor_behaviour(
         // Draw line2: 1.2s
         if (line2)
         {
+          printf("%s\n", line2->c_str());
           u8g2_SetFont(&u8g2, u8g2_font_helvB14_tr);
-          //u8g2_DrawStr(&u8g2, 0, 31, duration_str);
-          //u8g2_DrawStr(&u8g2, 0, 31, email->c_str());
           u8g2_DrawStr(&u8g2, 0, 31, line2->c_str());
         }
 
