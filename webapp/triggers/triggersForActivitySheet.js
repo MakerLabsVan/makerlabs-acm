accf02071b67c802588e1730c79f1360337b9511
Paul Reimer <paul@p-rimes.net>
2018-07-03 12:00:43 -0700

    ✨ Add webapp trigger to freeze the Makerlabs ID column of the Activity sheet (copying value out of formula into cell)

--- webapp/triggers/triggersForActivitySheet.js
+++ webapp/triggers/triggersForActivitySheet.js
@@ -46,10 +46,62 @@ function triggerActivityProvideNewRowDefaultsOnChange(e) {
   }
 }
 
+function triggerActivityFreezeIdsOnEdit(e) {
+  var sheetName = 'Activity'
+
+  // Only consider events that provide a range
+  if (e && e.range && e.range.getSheet().getName() == sheetName) {
+    // Only consider Activity sheet, exclude the headers from edited range if they were touched
+    var editedRange = e.range
+    var newActivityRange = editedRange
+    newActivityRange = excludeHeadersFromRange(newActivityRange)
+    newActivityRange = excludeTemplateRowFromRange(newActivityRange)
+
+    var sheet = editedRange.getSheet()
+
+    // Figure out first and last columns for the entire row
+    var firstColumn = 1
+    var lastColumn = Number(sheet.getMaxColumns())
+    var numColumns = lastColumn - firstColumn + 1
+
+    // Figure out first and last rows, avoid processing headers
+    var firstEditedRow = Number(newActivityRange.getRow())
+    var lastEditedRow =
+      Number(newActivityRange.getNumRows()) + firstEditedRow - 1
+    var numEditedRows = Number(newActivityRange.getNumRows())
+
+    var freezeActivityIdsRange = sheet.getRange(
+      firstEditedRow,
+      firstColumn,
+      numEditedRows,
+      numColumns
+    )
+
+    if (freezeActivityIdsRange) {
+      freezeActivityIdsInRange(freezeActivityIdsRange)
+    }
+  }
+}
+
+function triggerActivityFreezeIdsOnChange(e) {
+  var sheetName = 'Activity'
+
+  if (e && e.changeType && e.changeType == 'INSERT_ROW') {
+    // Only consider Activity sheet data rows
+    var activityDataRowsRange = getDataRowsForSheet(sheetName)
+
+    if (activityDataRowsRange) {
+      freezeActivityIdsInRange(activityDataRowsRange)
+    }
+  }
+}
+
 function triggersForActivitySheetOnEdit(e) {
   triggerActivityProvideNewRowDefaultsOnEdit(e);
+  triggerActivityFreezeIdsOnEdit(e);
 }
 
 function triggersForActivitySheetOnChange(e) {
   triggerActivityProvideNewRowDefaultsOnChange(e);
+  triggerActivityFreezeIdsOnChange(e);
 }
