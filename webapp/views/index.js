ae7563d735398ef12603b37188e39b53d39f812c
Paul Reimer <paul@p-rimes.net>
2018-07-13 19:53:51 -0700

    🔥 Remove view-user-page route and views/viewUserPage.js handler in webapp/ component, now using Firebase hosting since Google Apps Script templates are not used.

--- webapp/views/index.js
+++ webapp/views/index.js
@@ -1,11 +1,8 @@
 function doGet(e) {
-  var route = e.parameter.href || 'view-user-page'
+  var route = e.parameter.href || 'view-user-fields'
 
   switch (route) {
     default:
-    case 'view-user-page':
-      return viewUserPage(e)
-      break;
     case 'view-user-fields':
       return viewUserFields(e)
       break;
