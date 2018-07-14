01addf0f9d7bd670ef0b197a64c45bc7cb96d299
Paul Reimer <paul@p-rimes.net>
2018-07-13 18:09:12 -0700

    ♻️ Return JSONP library as well as invoke JSONP callback in webapp/views/viewUsersFields response

--- webapp/views/viewUsersFields.js
+++ webapp/views/viewUsersFields.js
@@ -1,7 +1,12 @@
 function viewUserFields(req) {
   var fields = getUsersFields();
   var jsonContent = JSON.stringify(fields);
+  var jsonpContent = (
+    "var acm=window.acm=window.acm||{}; " +
+    "acm.userFields = " + jsonContent + "; " +
+    req.parameters.callback + "(" + jsonContent + ")"
+  );
 
-  return ContentService.createTextOutput(jsonContent)
-    .setMimeType(ContentService.MimeType.JSON);
+  return ContentService.createTextOutput(jsonpContent)
+    .setMimeType(ContentService.MimeType.JAVASCRIPT);
 }
