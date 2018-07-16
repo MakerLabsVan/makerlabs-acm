d41f1d1b3fa8243d04e6afd65368dd0faaa3b2d2
Paul Reimer <paul@p-rimes.net>
2018-07-16 14:14:00 -0700

    🔥 Remove unused Google Apps Script files from webapp/

--- webapp/triggers/triggersForUsersSheet.js
+++ webapp/triggers/triggersForUsersSheet.js
@@ -1,41 +1,3 @@
-// Detect an entry in a cell with a URL beginning with "http://" or "https://"
-// Convert it to a formula, "=IMAGE(...)"
-// Optionally select a valid low-res image if an Instagram link is detected
-// Store the user-provider text in a (hidden) column to the right of this one
-function triggerUsersConvertUrlsToImagesOnEdit(e) {
-  var sheetName = 'Users'
-
-  // Only run on edit events which provide a range
-  if (e && e.range && e.range.getSheet().getName() == sheetName) {
-    // Only consider edited ranges that have touched the Users sheet photo column
-    var editedRange = e.range
-    var userPhotosRange = getUserPhotosRange()
-    var convertUrlsToImagesRange = getRangeIntersection(
-      editedRange,
-      userPhotosRange
-    )
-
-    if (convertUrlsToImagesRange) {
-      convertUrlsToImagesInRange(convertUrlsToImagesRange)
-    }
-  }
-}
-
-function triggerUsersResizeRowsHeightOnChange(e) {
-  var sheetName = 'Users'
-
-  // Only run on change events of type "OTHER" (e.g. column resize)
-  if (e && e.changeType && e.changeType == 'OTHER') {
-    // Re-apply to the Users sheet rows, selecting only the photo column
-    // OnChange event does not provide any info about what changed
-    var userPhotosRange = getUserPhotosRange()
-
-    if (userPhotosRange) {
-      resizeRowHeightInRange(userPhotosRange)
-    }
-  }
-}
-
 function triggerUsersEnforceFormattingOnEdit(e) {
   var sheetName = 'Users'
 
@@ -67,44 +29,10 @@ function triggerUsersEnforceFormattingOnChange(e) {
   }
 }
 
-function triggerUsersEnforceCheckboxValidationsOnEdit(e) {
-  var sheetName = 'Users'
-
-  // Only run on edit events which provide a range
-  if (e && e.range && e.range.getSheet().getName() == sheetName) {
-    // Only consider Users sheet, exclude the headers from edited range if they were touched
-    var editedRange = e.range
-    var enforceCheckboxValidationsRange = excludeHeadersFromRange(editedRange)
-
-    if (enforceCheckboxValidationsRange) {
-      enforceCheckboxValidationsInRange(enforceCheckboxValidationsRange)
-    }
-  }
-}
-
-function triggerUsersEnforceCheckboxValidationsOnChange(e) {
-  var sheetName = 'Users'
-
-  // Only run on change events of type "OTHER" (e.g. data validation removal)
-  if (e && e.changeType && e.changeType == 'OTHER') {
-    // Re-apply to all Users sheet data rows
-    // OnChange event does not provide any info about what changed
-    var dataRowsRange = getDataRowsForSheet(sheetName)
-
-    if (dataRowsRange) {
-      //TODO: exclude template row from dataRowsRange
-      enforceCheckboxValidationsInRange(dataRowsRange)
-    }
-  }
-}
-
 function triggersForUsersSheetOnEdit(e) {
-  triggerUsersConvertUrlsToImagesOnEdit(e)
   triggerUsersEnforceFormattingOnEdit(e)
 }
 
 function triggersForUsersSheetOnChange(e) {
-  //triggerUsersResizeRowsHeightOnChange(e)
   triggerUsersEnforceFormattingOnChange(e)
-  triggerUsersEnforceCheckboxValidationsOnChange(e)
 }
--- webapp/utils/getSafeFieldName.js
+++
@@ -1,7 +0,0 @@
-function getSafeFieldName(unsafeFieldName) {
-  return unsafeFieldName
-    .replace(/[- \/]+/g, '_') // Replace known separators with '_'
-    .replace(/^([^A-Za-z])/, '_$1') // Prefix with '_' if not starting with a letter
-    .replace(/\W/g, '') // Remove all other non-alphanumeric
-    .toLowerCase() // Lowercase only
-}
