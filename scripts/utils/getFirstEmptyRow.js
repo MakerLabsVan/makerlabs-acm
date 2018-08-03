3d6525380922b2fcee9ad7568d17274680dc72db
Paul Reimer <paul@p-rimes.net>
2018-08-02 17:29:17 -0700

    ✨ Add helper function scripts/utils/getFirstEmptyRow.js
    To search through a Range and find the first entire row that is empty

---
+++ scripts/utils/getFirstEmptyRow.js
@@ -0,0 +1,56 @@
+function getFirstEmptyRow(range) {
+  const batchSize = 10;
+  if (range) {
+    const numRows = Number(range.getNumRows());
+    const numColumns = Number(range.getNumColumns());
+
+    // Iterate through the rows in batches
+    for (
+      var currentRowIdx = 0;
+      currentRowIdx < numRows;
+      currentRowIdx += batchSize
+    ) {
+      var thisBatchSize = batchSize;
+
+      // Adjust size if outside bounds
+      if (currentRowIdx + thisBatchSize > numRows) {
+        thisBatchSize = numRows - currentRowIdx;
+      }
+
+      var batchRange = range.offset(
+        currentRowIdx,
+        0,
+        thisBatchSize,
+        numColumns
+      );
+      var values = batchRange.getValues();
+
+      // Iterate through the batch rows
+      for (var row = 0; row < batchRange.getNumRows(); ++row) {
+        var rowHasData = false;
+        // Iterate through this row's columns
+        for (var col = 0; col < batchRange.getNumColumns(); ++col) {
+          // Skip this row if any data is present
+          if (values[row][col] != "") {
+            rowHasData = true;
+          }
+        }
+
+        // If all columns were empty, return that row
+        if (!rowHasData) {
+          // Figure out first, last rows
+          const firstRow = Number(range.getRow());
+          const lastRow = numRows + firstRow - 1;
+
+          var firstEmptyRow = firstRow + currentRowIdx + row;
+          // Only consider the first empty row if it occurred before the end
+          if (firstEmptyRow <= lastRow) {
+            return firstEmptyRow;
+          }
+        }
+      }
+    }
+  }
+
+  return null;
+}
