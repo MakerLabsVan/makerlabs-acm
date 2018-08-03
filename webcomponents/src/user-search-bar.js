8e372085c05c7c9188a6c5ee8e22ec8b98f12162
Paul Reimer <paul@p-rimes.net>
2018-08-02 19:09:48 -0700

    🎨 Reformat webcomponents/user-search-bar

--- webcomponents/src/user-search-bar.js
+++ webcomponents/src/user-search-bar.js
@@ -1,16 +1,9 @@
 import {LitElement, html} from "@polymer/lit-element";
-
 import "@vaadin/vaadin-combo-box/theme/material/vaadin-combo-box.js";
-
 class UserSearchBar extends LitElement {
   static get properties() {
-    return {
-      items: {
-        type: Array,
-      },
-    };
+    return {items: {type: Array}};
   }
-
   constructor() {
     super();
     this.items = this.items || [];
@@ -28,18 +21,29 @@ class UserSearchBar extends LitElement {
       >
         <template>
           <paper-icon-item>
-            <img src="[[item.Photo]]" style="border-radius: 50%; width: 48px; height: 48px; [[item.imageStyle]]" slot="item-icon">
-            <iron-icon icon="inbox" class="big" style="[[item.iconStyle]]" slot="item-icon"></iron-icon>
+            <img
+              src="[[item.Photo]]"
+              style="border-radius: 50%; width: 48px; height: 48px; [[item.imageStyle]]"
+              slot="item-icon"
+            >
+            <iron-icon
+              icon="inbox"
+              class="big"
+              style="[[item.iconStyle]]"
+              slot="item-icon"
+            ></iron-icon>
             <paper-item-body two-line="" style="min-height: 0">
               <div style="[[item.style]]">[[item.Name]]</div>
-              <div secondary="" style="[[item.secondaryStyle]]">[[item.Email]]</div>
+              <div secondary="" style="[[item.secondaryStyle]]">
+                [[item.Email]]
+              </div>
             </paper-item-body>
           </paper-icon-item>
+
         </template>
       </vaadin-combo-box>
-    `;
+      `;
   }
-
   handleSearchChanged() {
     const searchbox = this.shadowRoot.getElementById("searchbox");
     if (searchbox && searchbox.selectedItem) {
@@ -49,5 +53,4 @@ class UserSearchBar extends LitElement {
     }
   }
 }
-
 customElements.define("user-search-bar", UserSearchBar);
