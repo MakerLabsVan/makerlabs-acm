daf41f6c71efc9a65824eda9c6156da09d9a9d7c
Paul Reimer <paul@p-rimes.net>
2018-09-15 10:41:59 -0700

    🔧 Update webcomponents/sw-precache-config.js

--- webcomponents/sw-precache-config.js
+++ webcomponents/sw-precache-config.js
@@ -9,27 +9,41 @@
  */
 
 module.exports = {
-  staticFileGlobs: ["manifest.json", "src/**/*"],
+  staticFileGlobs: [
+    "manifest.json",
+    "src/**/*",
+    "node_modules/@material/layout-grid/dist/mdc.layout-grid.min.css",
+    "node_modules/web-animations-js/web-animations-next.min.js",
+    "node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js",
+    "node_modules/fetch-jsonp/build/fetch-jsonp.js",
+  ],
   runtimeCaching: [
     {
-      urlPattern: /\/@webcomponents\/webcomponentsjs\//,
-      handler: "fastest",
-    },
-    {
-      urlPattern: /\/web-animations-js\//,
-      handler: "fastest",
-    },
-    {
-      urlPattern: /\/fetch-jsonp\//,
-      handler: "fastest",
-    },
-    {
       urlPattern: /\/__\/firebase\//,
+      handler: "cacheFirst",
+    },
+    {
+      urlPattern: /^https:\/\/apis.google.com\/js\/api.js/,
       handler: "fastest",
     },
     {
       urlPattern: /^https:\/\/fonts.gstatic.com\//,
       handler: "fastest",
+    },
+    {
+      urlPattern: /\/google_apps_script_proxy\/.*?href=users-fields/,
+      handler: "fastest",
+    },
+    {
+      urlPattern: /https:\/\/drive.google.com\//,
+      handler: "cacheFirst",
+      options: {
+        cache: {
+          maxEntries: 1000,
+          name: "profile-photos-cache",
+          maxAgeSeconds: 604800,
+        },
+      },
     },
   ],
 };
