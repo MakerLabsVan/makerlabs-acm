857f402880ce267b5614b3713d1ba750ef5479a8
Paul Reimer <paul@p-rimes.net>
2019-02-15 16:57:42 -0800

    🔧 Add replace-in-file config files for test and production environments

---
+++ webcomponents/replace-in-file.test.js
@@ -0,0 +1,15 @@
+module.exports = {
+  from: [
+    /sheetId="[^"]*"/,
+    /fieldsUrl="[^"]*"/,
+    /nextMakerLabsIdUrl="[^"]*"/,
+    /profilePhotosFolderId="[^"]*"/
+  ],
+  to: [
+    'sheetId="1Sd3nYY34dllVUsakwU8hLnJQ1m-U50qZwQSxst2SKKg"',
+    'fieldsUrl="/google_apps_script_proxy/a/p-rimes.net/macros/s/AKfycbx6_nGBJJxIVs5GmL3pFAigEjQfgUGAqFgryu0cq5cndIY0vDA/exec?href=users-fields"',
+    'nextMakerLabsIdUrl="/google_apps_script_proxy/a/p-rimes.net/macros/s/AKfycbx6_nGBJJxIVs5GmL3pFAigEjQfgUGAqFgryu0cq5cndIY0vDA/exec?href=next-makerlabs-id"',
+    'profilePhotosFolderId="172rkqKdtwvApTtRXbRxxrhdmP-wtbXad"'
+  ],
+  files: "build/es6-bundled/view-user-page.html"
+};
