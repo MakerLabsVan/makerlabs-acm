273124a782ac66a98eec5eb3e4a73fec7503601e
Paul Reimer <paul@p-rimes.net>
2019-02-15 20:08:15 -0800

    ✨ Add default firmware programming flow if no subcommand is specified

--- bin/run_firmware_for_machine.sh
+++ bin/run_firmware_for_machine.sh
@@ -6,14 +6,27 @@ IFS=$'\n\t'
 IDF_PATH="$(pwd)/esp-idf"
 
 # Extract MACHINE_NAME as first arg, then remove it
-MACHINE_NAME="${1:-default}"
+MACHINE_NAME="${1:?Must specific build-<MACHINE_NAME> name}"
 shift
 
 BUILD_DIR="$(pwd)/build-${MACHINE_NAME}"
 
-$IDF_PATH/tools/idf.py \
-  --no-warnings \
-  -G Ninja \
-  -DSDKCONFIG="${BUILD_DIR}/sdkconfig" \
-  -B "${BUILD_DIR}" \
-  $@
+if [ $# -eq 0 ]; then
+  # No args runs this sequence
+  $0 ${MACHINE_NAME} erase_flash
+  $0 ${MACHINE_NAME} flash
+  $0 ${MACHINE_NAME} flash_fatfs
+  $0 ${MACHINE_NAME} monitor
+elif [ "$1" = "flash_fatfs" ]; then
+  cd "${BUILD_DIR}"
+  ninja flash_fatfs
+  cd -
+else
+  # Run a specific idf.py command
+  $IDF_PATH/tools/idf.py \
+    --no-warnings \
+    -G Ninja \
+    -DSDKCONFIG="${BUILD_DIR}/sdkconfig" \
+    -B "${BUILD_DIR}" \
+    $@
+fi
