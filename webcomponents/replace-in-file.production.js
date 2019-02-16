32c9372519a45c91579b4a0f83872ad1c6b7d1be
Paul Reimer <paul@p-rimes.net>
2019-02-15 17:00:20 -0800

    🐛 Use all lowercase prop names in replace-in-file config

--- webcomponents/replace-in-file.production.js
+++ webcomponents/replace-in-file.production.js
@@ -1,15 +1,15 @@
 module.exports = {
   from: [
-    /sheetId="[^"]*"/,
-    /fieldsUrl="[^"]*"/,
-    /nextMakerLabsIdUrl="[^"]*"/,
-    /profilePhotosFolderId="[^"]*"/
+    /sheetid="[^"]*"/,
+    /fieldsurl="[^"]*"/,
+    /nextmakerlabsidurl="[^"]*"/,
+    /profilephotosfolderid="[^"]*"/
   ],
   to: [
-    'sheetId="1j4SIaQWzUO7LrVK_94p1n_SR70Og9ZycUm3GHVMY0zE"',
-    'fieldsUrl="/google_apps_script_proxy/a/makerlabs.com/macros/s/AKfycbzdJ6CbHD0Uqn2H2HEUyl13w2UPr0c0ztun8jdXykUh88mUE9ly/exec?href=users-fields"',
-    'nextMakerLabsIdUrl="/google_apps_script_proxy/a/makerlabs.com/macros/s/AKfycbzdJ6CbHD0Uqn2H2HEUyl13w2UPr0c0ztun8jdXykUh88mUE9ly/exec?href=next-makerlabs-id"',
-    'profilePhotosFolderId="1a3fKhzE1FCiJPV79acM7pkUp6WLM1w6s"'
+    'sheetid="1j4SIaQWzUO7LrVK_94p1n_SR70Og9ZycUm3GHVMY0zE"',
+    'fieldsurl="/google_apps_script_proxy/a/makerlabs.com/macros/s/AKfycbzdJ6CbHD0Uqn2H2HEUyl13w2UPr0c0ztun8jdXykUh88mUE9ly/exec?href=users-fields"',
+    'nextmakerlabsidurl="/google_apps_script_proxy/a/makerlabs.com/macros/s/AKfycbzdJ6CbHD0Uqn2H2HEUyl13w2UPr0c0ztun8jdXykUh88mUE9ly/exec?href=next-makerlabs-id"',
+    'profilephotosfolderid="1a3fKhzE1FCiJPV79acM7pkUp6WLM1w6s"'
   ],
   files: "build/es6-bundled/view-user-page.html"
 };
--- webcomponents/replace-in-file.test.js
+++ webcomponents/replace-in-file.test.js
@@ -1,15 +1,15 @@
 module.exports = {
   from: [
-    /sheetId="[^"]*"/,
-    /fieldsUrl="[^"]*"/,
-    /nextMakerLabsIdUrl="[^"]*"/,
-    /profilePhotosFolderId="[^"]*"/
+    /sheetid="[^"]*"/,
+    /fieldsurl="[^"]*"/,
+    /nextmakerlabsidurl="[^"]*"/,
+    /profilephotosfolderid="[^"]*"/
   ],
   to: [
-    'sheetId="1Sd3nYY34dllVUsakwU8hLnJQ1m-U50qZwQSxst2SKKg"',
-    'fieldsUrl="/google_apps_script_proxy/a/p-rimes.net/macros/s/AKfycbx6_nGBJJxIVs5GmL3pFAigEjQfgUGAqFgryu0cq5cndIY0vDA/exec?href=users-fields"',
-    'nextMakerLabsIdUrl="/google_apps_script_proxy/a/p-rimes.net/macros/s/AKfycbx6_nGBJJxIVs5GmL3pFAigEjQfgUGAqFgryu0cq5cndIY0vDA/exec?href=next-makerlabs-id"',
-    'profilePhotosFolderId="172rkqKdtwvApTtRXbRxxrhdmP-wtbXad"'
+    'sheetid="1Sd3nYY34dllVUsakwU8hLnJQ1m-U50qZwQSxst2SKKg"',
+    'fieldsurl="/google_apps_script_proxy/a/p-rimes.net/macros/s/AKfycbx6_nGBJJxIVs5GmL3pFAigEjQfgUGAqFgryu0cq5cndIY0vDA/exec?href=users-fields"',
+    'nextmakerlabsidurl="/google_apps_script_proxy/a/p-rimes.net/macros/s/AKfycbx6_nGBJJxIVs5GmL3pFAigEjQfgUGAqFgryu0cq5cndIY0vDA/exec?href=next-makerlabs-id"',
+    'profilephotosfolderid="172rkqKdtwvApTtRXbRxxrhdmP-wtbXad"'
   ],
   files: "build/es6-bundled/view-user-page.html"
 };
